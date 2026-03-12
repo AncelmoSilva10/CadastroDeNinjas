7e4f3f81d9016cf4c92e93a66dbd3273c100920a
AncelmoSilva10 <ancelmo.ssousa@sptech.school>
2026-03-11 22:30:19 -0300

    Finalizando o Back-End, conectando ao Banco de Dados MySQL

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
@@ -1,13 +1,20 @@
 package dev.java10x.CadastroDeNinja.Missoes;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -20,9 +27,7 @@ public class MissoesModel {
 
     // @OneToMany - uma missao pode ter varios ninjas
     @OneToMany (mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninja;
-
-
-
 
 }
--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
@@ -1,14 +1,24 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")// Define o prefixo da rota
+@CrossOrigin(origins = "*")
 public class NinjaController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Essa é minha primeira mensagem nessa rota";
+    @Autowired
+    private NinjaService ninjaService;
+
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
     }
 
+    @PostMapping("/criar")
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+        return ninjaService.salvarNinja(ninja);
+    }
 }
